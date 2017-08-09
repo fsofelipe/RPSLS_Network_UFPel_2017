a6cf0f7ca59a287d85f79df58c62a18ab2bdc197
fsofelipe <felipe.oliveira.tk@gmail.com>
2017-08-09 18:56:34 -0300

    Add files via upload

--- Server/src/server/Main.java
+++ Server/src/server/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package server;
 
 
@@ -14,3 +15,21 @@ public class Main {
 
     }
 }
+=======
+package server;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception  {
+        Game teste = new Game(12, 34);
+
+        System.out.println(teste.makePlay(12, HandStatus.ROCK));
+        System.out.println(teste.makePlay(34, HandStatus.LIZARD));
+
+        Server gameServer = new Server();
+        gameServer.ServerStart();
+        
+
+    }
+}
+>>>>>>> 6652df7ee9c4abb6259b2f0c55c838571dbe0d8a
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,3 +1,124 @@
+<<<<<<< HEAD
+package server;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+
+public class Server {
+    private Timer timeOut;
+    private boolean kick;
+     
+    public Server() {
+        lastUser = 0;
+        kick = false;
+    }
+    
+    
+    private int lastUser;
+    
+    ArrayList <Integer> connected = new ArrayList<Integer>();
+
+    private void kickPlayer(Socket client){
+        
+    }
+    
+    private void createNewLobby(Socket socketUserA, Socket socketUserB) {
+        try{
+            System.out.println("Server | New lobby:");
+            BufferedReader socketReaderA = new BufferedReader(new InputStreamReader(socketUserA.getInputStream()));
+            BufferedWriter socketWriterA = new BufferedWriter(new OutputStreamWriter(socketUserA.getOutputStream()));
+            
+            BufferedReader socketReaderB = new BufferedReader(new InputStreamReader(socketUserB.getInputStream()));
+            BufferedWriter socketWriterB = new BufferedWriter(new OutputStreamWriter(socketUserB.getOutputStream()));
+
+            //tell both players it is their time to play
+            //sempre que o client receber 0, deve fazer uma nova jogada
+            socketWriterA.write(0 + "\n");
+            socketWriterA.flush();
+            
+            socketWriterB.write(0 + "\n");
+            socketWriterB.flush();
+            
+            //WAITS FOR A REPLAY FROM THE CLIENT
+            /*
+            while (((inMsgA = socketReaderA.readLine()) != null) && ((inMsgB = socketReaderB.readLine()) != null)) {
+                System.out.println("Received from  client A: " + inMsgA);
+                System.out.println("Received from  client B: " + inMsgB);
+                lastUser++;
+                
+                connected.add(lastUser);
+                
+                socketWriterA.write(lastUser + "\n");
+                socketWriterA.flush();
+            }
+            */
+            
+            
+            //replay the winner ID
+            
+            //close sockets
+            socketUserA.close();
+            socketUserB.close();
+           
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    
+    public void ServerStart()throws Exception{
+        ServerSocket serverSocket = new ServerSocket(60000, 100, InetAddress.getByName("localhost"));
+        System.out.println("Server started  at:  " + serverSocket);
+        
+        while (true) {
+            System.out.println("Waiting for a  connection...");
+
+            final Socket socketUserA = serverSocket.accept();
+            System.out.println("Received a  connection from  " + socketUserA);
+            
+            //ANSWER WITH THE PLAYER ID;
+            BufferedWriter answUserA = new BufferedWriter(new OutputStreamWriter(socketUserA.getOutputStream()));
+            lastUser++;
+            answUserA.write(lastUser+"\n");
+            answUserA.flush();
+            
+            ///////////////////////////////////////////////////////////////////////////////////////////
+            //CREATE LOBBY WITH TIMEOUT
+            
+            
+            
+            //if the time runs out kick the first player
+            if (kick == true){
+                System.out.println("KICKA");
+            }
+            
+            ///////////////////////////////////////////////////////////////////////////////////////////
+            
+            //WAITS FOR THE OTHER USER
+            final Socket socketUserB = serverSocket.accept();
+            System.out.println("Received a  connection from  " + socketUserB);
+            
+            //ANSWER WITH THE PLAYER ID;
+            BufferedWriter answUserB = new BufferedWriter(new OutputStreamWriter(socketUserB.getOutputStream()));
+            lastUser++;
+
+            answUserB.write(lastUser + "\n");
+            answUserB.flush();
+            
+            //START GAME IN A NEW THREAD
+            if ((connected.size() % 2) == 0){
+                Runnable runnable = () -> createNewLobby(socketUserA, socketUserB);
+                new Thread(runnable).start(); // start a new thread
+            }
+            
+            
+        }
+    }
+    
+    
+=======
 package server;
 import java.io.*;
 import java.net.*;
@@ -73,4 +194,5 @@ public class Server {
     }
     
     
+>>>>>>> 6652df7ee9c4abb6259b2f0c55c838571dbe0d8a
 }
