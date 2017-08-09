150a7730a87d2103d1d8408e058ae449097f1bc5
fsofelipe <felipe.oliveira.tk@gmail.com>
2017-08-09 18:56:57 -0300

    Add files via upload

--- Client_runner/src/client_runner/ClientTCP.java
+++ Client_runner/src/client_runner/ClientTCP.java
@@ -1,3 +1,46 @@
+<<<<<<< HEAD
+package client_runner;
+
+import java.io.*;
+import java.net.*;
+
+public class ClientTCP {
+    
+    
+    public static void exec(String myName, int ipPort) throws Exception {
+        Socket socket = new Socket("localhost", ipPort);
+
+        System.out.println("Started client  socket at " + socket.getLocalSocketAddress());
+
+        BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+
+
+        String outMsg = null;
+
+
+        //while ((outMsg = consoleReader.readLine()) != null) {
+
+          // Add a new line to the message to the server,
+          // because the server reads one line at a time.
+          socketWriter.write(myName+'\n');
+          socketWriter.flush();
+
+          // Read and display the message from the server
+          String inMsg = socketReader.readLine();
+          System.out.println(inMsg);
+          System.out.println(); // Print a blank line
+
+          
+          while (true)
+        //}
+        socket.close();
+    } 
+
+}
+
+=======
 package client_runner;
 
 import java.io.*;
@@ -37,3 +80,4 @@ public class ClientTCP {
 
 }
 
+>>>>>>> 6652df7ee9c4abb6259b2f0c55c838571dbe0d8a
--- Client_runner/src/client_runner/Client_runner.java
+++ Client_runner/src/client_runner/Client_runner.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package client_runner;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -18,3 +19,25 @@ public class Client_runner {
     }
     
 }
+=======
+package client_runner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Client_runner {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception{
+        System.out.println("Please informe the user name: ");
+        BufferedReader getName = new BufferedReader(new InputStreamReader(System.in));
+        String name = getName.readLine();
+        ClientTCP.exec(name, 60000);
+        // TODO code application logic here
+    
+    }
+    
+}
+>>>>>>> 6652df7ee9c4abb6259b2f0c55c838571dbe0d8a
